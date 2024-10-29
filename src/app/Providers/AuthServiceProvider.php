aa4d503e3d24f48b7c303bd281a07f8e2e19f552
oohashitakayuki <ssg675vl@docomo.ne.jp>
2024-10-29 11:28:41 +0900

    メールのメッセージを追加

--- src/app/Providers/AuthServiceProvider.php
+++ src/app/Providers/AuthServiceProvider.php
@@ -31,7 +31,8 @@ class AuthServiceProvider extends ServiceProvider
             return (new MailMessage())
                 ->subject(__('Verify Email Address'))
                 ->line(__('Click the button below to verify your email address.'))
-                ->action(__('Verify Email Address'), $url);
+                ->action(__('Verify Email Address'), $url)
+                ->line(__('If you did not create an account, no further action is required.'));
         });
     }
 }
