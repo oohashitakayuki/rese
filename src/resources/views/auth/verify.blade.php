d968034ed7bf0980e8baab9567e1e0e3c319d057
oohashitakayuki <ssg675vl@docomo.ne.jp>
2024-11-07 16:28:45 +0900

    ビューの修正

--- src/resources/views/auth/verify.blade.php
+++ src/resources/views/auth/verify.blade.php
@@ -5,18 +5,17 @@
 @endsection
 
 @section('content')
-<div class="container">
-  <div class="card">
-    <div class="card__heading">メールアドレスの確認</div>
-
-    <div class="card__content">
-      ご登録いただいたメールアドレスに確認用のメールを送信しました。メールをご確認ください。<br>
-      もし確認用メールが送信されていない場合は、下記をクリックしてください。<br>
-      <form method="post" action="{{ route('verification.send') }}">
-        @csrf
-        <button type="submit">確認メールを再送信する</button>
-      </form>
+<div class="verify-page">
+  <div class="verify-page__heading">メールアドレスの確認</div>
+  <div class="verify-page__inner">
+    <div class="verify-page__message">
+      <p>ご登録いただいたメールアドレスに確認用のメールを送信しました。<br>メールをご確認ください。</p>
+      <p>もし確認用メールが送信されていない場合は、下記をクリックしてください。</p>
     </div>
+    <form class="verify-page__button" method="post" action="{{ route('verification.send') }}">
+      @csrf
+      <button class="verify-page__button-submit btn" type="submit">確認メールを再送信する</button>
+    </form>
   </div>
 </div>
 @endsection
--- src/resources/views/done.blade.php
+++ src/resources/views/done.blade.php
@@ -8,8 +8,8 @@
 <div class="done-page">
   <div class="done-page__inner">
     <p class="done-page__message">ご予約ありがとうございます</p>
-    <form class="done-page__form" action="/" method="get">
-      <button class="done-page__btn">戻る</button>
+    <form class="done-page__button" action="/" method="get">
+      <button class="done-page__button-submit btn">戻る</button>
     </form>
   </div>
 </div>
