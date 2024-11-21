8c7d89c7441b724aed8b006d005142cf3ea604de
oohashitakayuki <ssg675vl@docomo.ne.jp>
2024-11-21 15:56:50 +0900

    input type=dateで過去の日付を選択不可にする設定を追加

--- src/public/js/reservation.js
+++ src/public/js/reservation.js
@@ -1,4 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
+window.onload = function(){
+    var getToday = new Date();
+    var y = getToday.getFullYear();
+    var m = getToday.getMonth() + 1;
+    var d = getToday.getDate();
+    var today = y + "-" + m.toString().padStart(2,'0') + "-" + d.toString().padStart(2,'0');
+    document.getElementById("reservation-date").setAttribute("value",today);
+    document.getElementById("reservation-date").setAttribute("min",today);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
   const dateInput = document.getElementById('reservation-date');
   const timeSelect = document.getElementById('reservation-time');
   const numberSelect = document.getElementById('reservation-number');
